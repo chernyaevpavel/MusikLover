1f9f8ea089233b889d0f8de40130b9cdc464e8e4
Pavel Chernyaev <pawliklist@yandex.ru>
2023-12-29 00:53:03 +0400

    refactor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,17 @@
 import kotlin.math.roundToInt
 
 fun main() {
-    val amount = 1500
+    val amount = 8000
     val isVipClient = true
     val percent = 5.0
     val vipPercent = 1.0
 
     var sale = 0.0
-    if (amount >= 1001 && amount <= 10_000) {
+    if (amount in 1001..10_000) {
         sale = 100.0
     } else if (amount > 10_001) {
         sale = (amount * percent).roundToInt() / 100.0
     }
-    if (isVipClient) {
-        sale += (amount * vipPercent).roundToInt() / 100.0
-    }
+    if (isVipClient) sale += (amount * vipPercent).roundToInt() / 100.0
     println(sale)
 }
